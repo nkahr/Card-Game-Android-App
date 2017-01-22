2f5905a85dea757c53aededed171b2b576dc042d
Nina Kahr <ntkahr@gmail.com>
2017-01-22 13:13:19 +0000

    make main activity with a blackjack button

---
+++ app/src/main/java/example/codeclan/com/cardgame/BlackjackActivity.java
@@ -0,0 +1,11 @@
+package example.codeclan.com.cardgame;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 22/01/2017.
+ */
+
+public class BlackjackActivity extends AppCompatActivity{
+
+}
--- app/src/main/java/example/codeclan/com/cardgame/MainActivity.java
+++ app/src/main/java/example/codeclan/com/cardgame/MainActivity.java
@@ -1,9 +1,34 @@
 package example.codeclan.com.cardgame;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by user on 22/01/2017.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity{
 
+    Button playBlackjackButton;
+    Intent playBlackjackIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_layout);
+
+        playBlackjackButton = (Button) findViewById(R.id.play_blackjack_button);
+        playBlackjackIntent = new Intent(MainActivity.this, BlackjackActivity.class);
+
+        Log.d(getClass().toString(), "/nONCREATE WAS CALLED");
+    }
+
+    public void onBlackjackButtonPressed(View view) {
+        startActivity(playBlackjackIntent);
+        Log.d(getClass().toString(), "onBlackjackButtonPressed was called.");
+    }
 }
